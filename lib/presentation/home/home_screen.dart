fd4331ae36a0dff73c4a81c73e5a519888aa6094
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-05-20 17:22:15 +0700

    ui

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -6,11 +6,42 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Todos'),
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.filter_list),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Center(
         child: Container(
           child: Text('Todo'),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.list,
+            ),
+            title: Text('Todos'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.trending_up,
+            ),
+            title: Text('Stats'),
+          ),
+        ],
       ),
     );
   }
